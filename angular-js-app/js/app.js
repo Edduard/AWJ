b034b06b853ec7ba1f0afcb26e6b4a69bb6f10b5
SpedsT <spedst@yahoo.com>
2016-01-08 16:19:51 +0200

    Am facut tema 5

--- angular-js-app/js/app.js
+++ angular-js-app/js/app.js
@@ -1,6 +1,23 @@
 var app = angular.module('blog', [ ]);
 
-app.controller('HomeController', ['$scope', function($scope) {
+app.controller('PersoanaController', function($scope,$http) {
   $scope.helloWorld = 'Aplicatii Web cu suport Java!';
+    
+    var url = "http://localhost:8080/persoana";
+    $http.get(url).success(function(response){
+        $scope.persoane = response;
+        console.log($scope.persoane);
+    })
+    
+});
 
-}]);
+app.controller('MasinaController', function($scope,$http) {
+  $scope.helloWorld = 'Aplicatii Web cu suport Java!';
+    
+    var url = "http://localhost:8080/masina";
+    $http.get(url).success(function(response){
+        $scope.masini = response;
+        console.log($scope.masini);
+    })
+    
+});
